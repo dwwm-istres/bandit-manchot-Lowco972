52889fb2de61ac000ceff86017c6a1b87e5897ff
Mathias - Afpa Istres <75681787+mathias-afpa@users.noreply.github.com>
2021-01-18 15:28:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Le Bandit manchot
+### Signification
+Jeu de hasard, machine à sous que l'on trouve dans les casinos
+
+### Origine
+Le terme vient de l'argot et fait référence au levier, et donc au bras unique, de la machine que l'on devait actionner afin que les rouleaux se mettent à tourner.
---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Ici le JS
+/*
+    Il doit y avoir un credit de 50€ au démarrage, cette information doit afficher dans un message au démarrage. 
+    Vous devez pouvoir augmenter ou diminuer la mise, qui est de 5€ par défaut
+    Lorsqu'on clique sur "jouer" les rouleaux doivent defilé et afficher une image aléatoire, les images sont dans le dossier img
+    Le jeux doit calculer votre gain ou perte.
+    Le gain ou la perte doit être affiché dans un message sous la grande image
+*/
